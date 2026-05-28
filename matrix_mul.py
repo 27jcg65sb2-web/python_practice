b12dcfcd2922ec2c1ceb40142ac5c78f7694db8c
鈴木俊平 <suzukishunpei@suzukishunpeinoMacBook-Air.local>
2026-05-28 21:38:37 +0900

    refactor matrix operations

--- matrix_mul.py
+++ matrix_mul.py
@@ -1,4 +1,38 @@
+def can_multiply(A,B):
+    return len(A[0]) == len(B)
+def matrix(A):
+    if len(A) == 0:
+        return False
+    r=len(A[0])
+    for row in A:
+        if len(row) != r:
+            return False
+        
+    return True
+def shape(A):
+    if not matrix(A):
+        raise ValueError("not matrix")
+    return (len(A),len(A[0]))
+def zeros(n,m):
+    y=[]
+    for i in range(n):
+        x=[]
+        for l in range(m):
+            x.append(0)
+        y.append(x)
+    return y
+
+
+
 def mat_mul(A,B):
+    if not can_multiply:
+        raise ValueError("matrix size mismatch")
+    if not matrix(A):
+        raise ValueError("A is not matrix")
+    if not matrix(B):
+        raise ValueError("B is not matrix")
+    if len(A[0]) != len(B):
+        raise ValueError("matrix size missmatch")
     l=len(A)
     d=len(B)
     c=len(B[0])
